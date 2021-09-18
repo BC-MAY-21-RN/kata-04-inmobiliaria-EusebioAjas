6d64ec094cb10197e4b60fd25e2716a2a961fab9
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2021-09-18 17:50:59 -0500

    Add house items and list

--- App.js
+++ App.js
@@ -6,107 +6,132 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type { Node } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { View, Text, FlatList, StyleSheet, StatusBar, ImageBackground } from 'react-native';
+import inmobiliariaData from './src/data/inmobiliariaData.json';
 
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+const CustomImage = ({ imgUrl, rate }) => (
+  <View>
+    <ImageBackground source={{ uri: imgUrl }} style={styles.image} imageStyle={{ borderRadius: 20 }}>
+      <RateContainer rate={rate} />
+    </ImageBackground>
+  </View >
+)
+
+const RateContainer = ({ rate }) => (
+  <View style={styles.rateContainer}>
+    <Icon name="star" size={20} color="#efbb31" />
+    <Text style={{ color: '#7b622f', marginLeft: 5, fontWeight: 'bold' }}>{rate}</Text>
+  </View>
+)
+
+const Item = ({ props }) => (
+  <View style={styles.item}>
+    <View style={styles.itemSection}>
+      <CustomImage style={styles.itemSection} imgUrl={props.imgUrl} rate={props.rate} />
     </View>
-  );
-};
+    <View style={styles.itemSection}>
+      <View>
+        <Text style={styles.title}>{props.name}</Text>
+      </View>
+      <View style={styles.detailContainer}>
+        <Icon name="location-on" size={20} />
+        <Text>{props.address}</Text>
+      </View>
+      <View style={styles.detailContainer}>
+        <View style={styles.detailItem}>
+          <Icon name="bathtub" size={20} />
+          <Text style={styles.detailText}>{props.rooms}</Text>
+        </View>
+        <View style={styles.detailItem}>
+          <Icon name="king-bed" size={20} />
+          <Text style={styles.detailText}>{props.bathrooms}</Text>
+        </View>
+        <View style={styles.detailItem}>
+          <Icon name="crop-square" size={20} />
+          <Text style={styles.detailText}>{props.surface}</Text>
+        </View>
+      </View>
+      <Text style={styles.title}>{props.rentalCostbyMonth}</Text>
+      <View style={styles.favoriteIcon}>
+        <Icon name="favorite" size={30} color="#00b076" />
+      </View>
+    </View >
+  </View>
+)
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [data] = useState(inmobiliariaData);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Editing <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next: and replace some text
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Item props={item} />}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  item: {
+    backgroundColor: '#f5fdff',
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'stretch',
+    borderRadius: 20,
+    marginHorizontal: 25,
+    marginVertical: 5,
+    padding: 2,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  itemSection: {
+    flex: 0.5,
+    padding: 4,
+    justifyContent: 'space-between'
   },
-  highlight: {
-    fontWeight: '700',
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold'
   },
-});
+  image: {
+    height: 160,
+    width: 150,
+  },
+  rateContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fbedbb',
+    borderRadius: 30,
+    marginHorizontal: 30,
+    marginTop: '75%',
+    padding: 10,
+  },
+  detailContainer: {
+    flexDirection: 'row',
+  },
+  detailItem: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 4,
+  },
+  detailText: {
+    fontWeight: 'bold',
+    marginLeft: 5
+  },
+  favoriteIcon: {
+    alignSelf: 'flex-end',
+  }
+})
 
 export default App;
+
